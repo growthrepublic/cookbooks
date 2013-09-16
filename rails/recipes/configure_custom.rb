acfeffbc74fef2874bf1c2d7be57bb22e55dfa9f
rabsztok <rabsztok@gmail.com>
2013-09-16 10:00:59 +0200

    modified rails

---
+++ rails/recipes/configure_custom.rb
@@ -0,0 +1,41 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/application.rb" do
+    source "application.rb.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:environment => deploy[:rails_env])
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/newrelic.yml" do
+    source "newrelic.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:newrelic => deploy[:newrelic], :environment => deploy[:rails_env])
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+end
