f829510d65a9e87d65c9e0326885a9a3d00c2a98
rabsztok <rabsztok@gmail.com>
2013-09-16 09:48:37 +0200

    modified rails

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -42,6 +42,21 @@ node[:deploy].each do |application, deploy|
     end
   end
 
+  template "#{deploy[:deploy_to]}/shared/config/newrelic.yml" do
+    source "newrelic.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:newrelic => deploy[:newrelic], :environment => deploy[:rails_env])
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
   template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
     source "memcached.yml.erb"
     cookbook 'rails'
