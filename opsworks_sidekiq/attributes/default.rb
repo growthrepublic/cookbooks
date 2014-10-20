2353ca1f602a788da878b37103ad1b52012ce485
Michał Matyas <github@higher.lv>
2014-10-20 11:47:27 +0200

    Change opsworks_sidekiq to different fork

--- opsworks_sidekiq/attributes/default.rb
+++ opsworks_sidekiq/attributes/default.rb
@@ -5,6 +5,6 @@ default[:sidekiq] = {}
 node[:deploy].each do |application, deploy|
   default[:sidekiq][application.intern] = {}
   default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
-  default[:sidekiq][application.intern][:syslog] = false
+  default[:sidekiq][application.intern][:syslog] = true
 end
 
--- opsworks_sidekiq/recipes/deploy.rb
+++ opsworks_sidekiq/recipes/deploy.rb
@@ -15,21 +15,22 @@ node[:deploy].each do |application, deploy|
     path deploy[:deploy_to]
   end
 
-  include_recipe "opsworks_sidekiq::setup"
+  Chef::Log.debug("Running opsworks_sidekiq::setup for application #{application}")
+  node.set[:opsworks][:rails_stack][:recipe] = "opsworks_sidekiq::setup"
 
-  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-    cookbook "rails"
-    source "memcached.yml.erb"
-    mode 0660
-    owner deploy[:user]
-    group deploy[:group]
-    variables(:memcached => (deploy[:memcached] || {}), :environment => deploy[:rails_env])
+  opsworks_rails do
+    deploy_data deploy
+    app application
   end
 
-  node.set[:opsworks][:rails_stack][:restart_command] = node[:sidekiq][application][:restart_command]
-
+  Chef::Log.debug("Deploying Sidekiq Application: #{application}")
   opsworks_deploy do
     deploy_data deploy
     app application
+  end
+
+  Chef::Log.debug("Restarting Sidekiq Application: #{application}")
+  execute "restart Rails app #{application}" do
+    command "sleep 60 && #{node[:sidekiq][application][:restart_command]}"
   end
 end
